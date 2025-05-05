b6e3c448eaebd3e9417903712ef1fb03c052c956
David Schreiber-Ranner <davidschreiber@users.noreply.github.com>
2025-05-05 22:48:36 +0200

    Add motivational messages (#1)

---
+++ src/api/motivational-message/routes/motivational-message.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * motivational-message router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::motivational-message.motivational-message');
---
+++ src/api/motivational-message/services/motivational-message.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * motivational-message service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::motivational-message.motivational-message');
