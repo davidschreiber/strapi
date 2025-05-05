c034c5b345199fb9fb071db40470c986a42f664d
David Schreiber-Ranner <davidschreiber@users.noreply.github.com>
2025-05-05 22:45:54 +0200

    Add motivational messages

---
+++ src/api/motivational-message/controllers/motivational-message.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * motivational-message controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::motivational-message.motivational-message');
