1ebb8f7c395bd556d91dadef891042402c1b0d90
David Schreiber-Ranner <davidschreiber@users.noreply.github.com>
2025-05-06 21:28:42 +0200

    feat(motivational-message): add endpoint to fetch random message (#5)
    Add a new controller method to retrieve a random motivational message
    from the database. Implement a GET route at /motivational-messages/random
    to expose this functionality. This change enables clients to easily
    access a random motivational message without fetching all entries.

--- src/api/motivational-message/controllers/motivational-message.js
+++ src/api/motivational-message/controllers/motivational-message.js
@@ -6,4 +6,16 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::motivational-message.motivational-message');
+module.exports = createCoreController('api::motivational-message.motivational-message', ({ strapi }) => ({
+  async random(ctx) {
+    const count = await strapi.entityService.count('api::motivational-message.motivational-message');
+    const randomIndex = Math.floor(Math.random() * count);
+    const randomMessage = await strapi.entityService.findMany('api::motivational-message.motivational-message', {
+      start: randomIndex,
+      limit: 1,
+    });
+
+    return randomMessage[0];
+  } 
+
+}));
